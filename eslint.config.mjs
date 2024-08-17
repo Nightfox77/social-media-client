12d1676aa85c16e8fcb90ab156c121fefe9fa9a4
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-08-17 16:58:37 +0200

    config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import cypressPlugin from 'eslint-plugin-cypress';
+import jestPlugin from 'eslint-plugin-jest';
+import globals from 'globals';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      jest: jestPlugin,
+      cypress: cypressPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'cypress/no-unnecessary-waiting': 'off',
+    },
+  },
 ];
