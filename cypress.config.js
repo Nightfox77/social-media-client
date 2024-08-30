1c29f6985794b06d40a0da07e7b270081dfd1d2e
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-08-30 18:50:11 +0200

    updated cypress config for fixing commit issue

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // Placeholder usage to avoid ESLint errors
-      if (on) {
-      }
-      if (config) {
-      }
+    setupNodeEvents() {
+     
+      
     },
   },
 });
