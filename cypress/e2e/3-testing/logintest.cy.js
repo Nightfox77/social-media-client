794cc9925cb0446fe7875b3e0290fbeeb770821c
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-08-31 14:35:32 +0200

    cypress logout + add testcase login test

--- cypress/e2e/3-testing/logintest.cy.js
+++ cypress/e2e/3-testing/logintest.cy.js
@@ -1,12 +1,12 @@
 describe("Login Functionality", () => {
   it("should successfully log in with valid credentials and store a token", () => {
-    cy.visit("../../index.html");
+    cy.visit("/");
 
     cy.get("#registerModal")
       .should("be.visible")
       .within(() => {
         cy.wait(300);
-        cy.get("button[data-bs-target]").should("exist").click();
+        cy.get('button[data-bs-target="#loginModal"]').should("exist").click();
       });
 
     // Ensure the login form is visible
@@ -24,6 +24,32 @@ describe("Login Functionality", () => {
       const token = win.localStorage.getItem("token");
       expect(token).to.be.a("string");
       expect(token).not.to.be.empty;
+    });
+  });
+  it("should deny access and show errormessage when presented invalid login credentials", () => {
+    cy.visit("/");
+
+    cy.get("#registerModal")
+      .should("be.visible")
+      .within(() => {
+        cy.wait(300);
+        cy.get('button[data-bs-target="#loginModal"]').should("exist").click();
+      });
+
+    // Ensure the login form is visible
+    cy.get("#loginForm")
+      .should("be.visible")
+      .within(() => {
+        cy.get('input[name="email"]').wait(500).type("wrongmail@noroff.no");
+        cy.get('input[name="password"]').type("wronpassword");
+        cy.get("button").contains("Login").click();
+      });
+    cy.wait(500);
+    cy.get(".alert").should("be.visible");
+
+    cy.window().then((win) => {
+      const token = win.localStorage.getItem("token");
+      expect(token).to.be.null;
     });
   });
 });
--- cypress/e2e/3-testing/logouttest.cy.js
+++ cypress/e2e/3-testing/logouttest.cy.js
@@ -1,16 +1,31 @@
-describe("Logout Functionality", () => {
-  it("should successfully logout and delete the token", () => {
+describe("Login Functionality", () => {
+  before(() => {
     cy.visit("../../index.html");
 
-    cy.wait(300);
-    cy.get('input[id="loginEmail"]').type("testmail@noroff.no");
-    cy.get('input[id="loginPassword"]').type("whatever");
-    cy.get("#loginBtn").click();
-    cy.wait(500);
+    cy.get("#registerModal")
+      .should("be.visible")
+      .within(() => {
+        cy.wait(300);
+        cy.get('button[data-bs-target="#loginModal"]').should("exist").click();
+      });
+    cy.get("#loginForm")
+      .should("be.visible")
+      .within(() => {
+        cy.get('input[name="email"]').wait(500).type("testmail@noroff.no");
+        cy.get('input[name="password"]').type("whatever");
+        cy.get("button").contains("Login").click();
+      });
+
+    cy.wait(1000);
+    cy.get("body").should("have.class", "logged-in");
+  });
+  it("should successfully log out and remove the token ", () => {
+    cy.contains("button", "Logout").click();
+    cy.wait(1000);
+    cy.get("body").should("not.have.class", "logged-in");
     cy.window().then((win) => {
       const token = win.localStorage.getItem("token");
-      expect(token).to.be.a("string");
-      expect(token).not.to.be.empty;
+      expect(token).to.be.null;
     });
   });
 });
